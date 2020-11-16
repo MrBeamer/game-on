365ee3fb3fc2b23ce0422ca4b919abf010ba9fae
michael beamer <beamermichael@gmail.com>
2020-11-16 22:50:07 +0100

    reworked home-page, edited searchbar and changed background and title + added to nav bar rent a game and mario avatar

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   before_action :single_game, only: [:show, :edit, :update, :destroy]
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :home]
 
   def index
     @games = Game.all
