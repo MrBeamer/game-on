de7066bce2baaac462a1739944e71289e1836d48
yilei jiang <yilei.jiang0@gmail.com>
2020-11-16 15:30:57 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :games
+  
+  resources :rentals, only: [:new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:destroy]
+
 end
