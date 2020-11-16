50e80f192545d12902c47fab4ea3337575666ade
michael beamer <beamermichael@gmail.com>
2020-11-16 20:39:34 +0100

    edited navbar to fit in and connected different views through links

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   resources :games do
     resources :rentals, only: [:new, :create] do
       resources :reviews, only: [:new, :create]
