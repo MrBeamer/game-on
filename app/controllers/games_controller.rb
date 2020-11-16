a602ebc07172a0467a451f84bca95b389833b690
yilei jiang <yilei.jiang0@gmail.com>
2020-11-16 16:28:46 +0100

    added games controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,49 @@
+class GamesController < ApplicationController
+  before_action :single_game, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @games = Games.all
+  end
+
+  def show
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to @game
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @game.update (game_params)
+      redirect_to @game
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @game.destroy
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :description, :price_per_day, :category, :condition, :availability, :photo)
+  end
+  
+  def single_game
+    @game = Game.find(params[:id])
+  end
+
+end
