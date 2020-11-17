a0cdcfb112fe91b7ae1b679405484fb1b4da9ab5
michael beamer <beamermichael@gmail.com>
2020-11-17 08:50:11 +0100

    added faker gem and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database now...'
+# Game.destroy_all
+puts 'Database clean ✅'
+
+10.times do
+    Game.create(
+    title: Faker::Game.title,
+    category: Faker::Game.genre,
+    description: Faker::TvShows::RickAndMorty.quote,
+    price_per_day: 4,
+    condition: "new"
+  )
+end
+
+puts "Complete, created #{Game.count} games."
