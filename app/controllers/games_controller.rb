51c1069a7445f97c6d105de053b8a62bde33243f
michael beamer <beamermichael@gmail.com>
2020-11-18 11:38:05 +0100

    Add dotenv - Protect my secret data in .env file

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   before_action :single_game, only: [:show, :edit, :update, :destroy]
-  skip_before_action :authenticate_user!, only: [:index, :home]
+  skip_before_action :authenticate_user!, only: [:index, :home, :show]
 
   def index
     # @games = Game.all
