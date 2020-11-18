433cd19c2adb9410c982e980cd5a9829860a605f
michael beamer <beamermichael@gmail.com>
2020-11-18 20:57:30 +0100

    editing still index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -22,7 +24,17 @@ require("channels")
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
 
+
+
+
+
+
+// External imports
+import "bootstrap";
+
 import AOS from 'aos';
+
+
 
 AOS.init();
 
@@ -30,13 +42,12 @@ AOS.init({
   duration: 1200,
 })
 
-// External imports
-import "bootstrap";
-
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { loadDynamicBannerText } from '../components/banner';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  loadDynamicBannerText();
 });
