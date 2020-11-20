2e30519bffcef23034a1925496e58219c80a6749
michael beamer <beamermichael@gmail.com>
2020-11-20 16:12:51 +0100

    edited depedency

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,8 @@
 class Game < ApplicationRecord
   include PgSearch::Model
   STATUS = %w(new used)
-
   belongs_to :user
-  has_one :rental
+  has_one :rental, dependent: :destroy
   has_one_attached :photo
   validates :title, :description, :price_per_day, :category, :condition, presence: true
   validates :price_per_day, numericality: { only_integer: true }
